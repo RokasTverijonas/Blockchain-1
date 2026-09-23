a540967090b681a1a61714c6fae1257d9c841cdd
RokasTverijonas <tverijonasr@gmail.com>
2026-09-23 23:13:42 +0300

    Improve hash function for v0.1

--- src/MyHash.java
+++ src/MyHash.java
@@ -1,12 +1,43 @@
 public class MyHash {
-    public static int hash(byte[] input) {
-        int hash = 8;
+    public static String hash(byte[] input) {
+        byte[] hash = new byte[32];
 
-        for(byte i : input) {
-            hash ^= i;
-            hash *= 51;
+        int seed = 293847597;
+
+        for(int i = 0; i < hash.length; i++) {
+            seed = seed * 28 + i;
+
+            hash[i] = (byte) seed;
         }
-        
+
+        for(int i = 0; i < input.length; i++) {
+            int index = i % 32;
+
+            hash[index] ^= input[i];
+
+            hash[index] *= 51;
+        }
+
+        hash = mixing(hash);
+
+        String hex = "";
+
+        for(byte i : hash) {
+            hex += String.format("%02x", i & 0xff);
+        }
+        return hex;
+    }
+
+    private static byte[] mixing(byte[] hash) {
+        for (int i = 0; i < hash.length; i++) {
+            int next = (i + 1) % hash.length;
+            int previous = (i - 1 + hash.length) % hash.length;
+
+            hash[i] ^= hash[next];
+            hash[i] ^= hash[previous];
+            hash[i] *= 51;
+
+        }
         return hash;
     }
 }
